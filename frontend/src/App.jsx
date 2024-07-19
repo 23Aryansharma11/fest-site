2cb83de5e6385bd8bf4606642ba651862b262858
Your Name <sujalverdhen1234@gmail.com>
2024-07-19 21:38:46 +0530

    file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './components/navbar/nav'
+// import Nav from './components/navbar/nav'
+import Home from './pages/Home.jsx'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-     <Nav/>
-    </>
+    <Router>
+    
+    <div className="App">
+    
+    <Routes>
+     
+     <Route exact path="/" element={<Home/>}/>
+     
+     </Routes>
+    </div>
+    
+    </Router>
   )
 }
 
