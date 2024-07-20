d1cd0e0213ccb4ffd2b45b0c112a49a8af8e8cb9
Aryan Sharma <23aryansharma11@gmail.com>
2024-07-20 16:58:21 +0530

    add utkarsh intro section

---
+++ frontend/src/components/UtkarshIntro/UtkarshIntro.jsx
@@ -0,0 +1,31 @@
+import SpinningLogo from "./SpinningLogo";
+
+const UtkarshIntro = () => {
+  return (
+    <main className=" bg-[#14110F] text-white flex justify-center gap-16 md:justify-between flex-col md:flex-row h-screen ">
+      <section className="flex justify-center items-center w-full md:w-1/2">
+        <SpinningLogo />
+      </section>
+      <section className="w-full md:w-1/2 flex flex-col  justify-center gap-5 md:gap-10 px-5 mr-20 text-left text-pretty">
+        <h1 className="text-4xl md:text-6xl font-bold ">Utkarsh</h1>
+        <h2 className="text-lg md:text-2xl md:leading-9">
+          Welcome to Utkarsh, the largest techno-cultural fest in Himachal
+          Pradesh! Hosted by{" "}
+          <span className="text-[#FCBA04] font-bold">
+            University Institude of Technology
+          </span>
+          , HPU, Utkarsh attracts over{" "}
+          <span className="text-[#FCBA04] font-bold">5000</span> participants
+          from North India. Since 2014, we have proudly completed 8 editions,
+          featuring over 25 competitions, lectures, and shows. <br />{" "}
+          <span className="text-[#FCBA04]">
+            Join us for an unforgettable experience with luminaries from around
+            the world.
+          </span>
+        </h2>
+      </section>
+    </main>
+  );
+};
+
+export default UtkarshIntro;
