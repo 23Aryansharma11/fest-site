96a9070920702cd168663e5dc079ddd1982297d5
23Aryansharma11 <129526491+23Aryansharma11@users.noreply.github.com>
2024-07-24 22:23:24 +0530

    add event page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,41 @@
-
-import './App.css'
+import "./App.css";
 // import Nav from './components/navbar/nav'
-import Home from './pages/Home.jsx'
-import {
- 
-  Routes,
-  Route,
-  Navigate,
-  
-} from "react-router-dom";
-import SignUp from './pages/SignUp.jsx';
-import Login from './pages/Login.jsx';
-import {Form} from './pages/Form.jsx';
-import { Toaster } from 'react-hot-toast';
-import { useAuthContext } from './context/authcontext.jsx';
-import EventInfo from './pages/EventInfo.jsx';
+import Home from "./pages/Home.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignUp from "./pages/SignUp.jsx";
+import Login from "./pages/Login.jsx";
+import { Form } from "./pages/Form.jsx";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/authcontext.jsx";
+import EventPage from "./components/events/EventPage/EventPage.jsx";
 
 function App() {
-const {authUser}=useAuthContext()
+  const { authUser } = useAuthContext();
 
   return (
-   
-    
     <div className="App">
-    
-    <Routes>
-      <Route  path="/" element={<Home/>}/>
-      <Route  path="/register" element={authUser?<Navigate to="/form"/>:<SignUp/>}/>
-      <Route  path="/login" element={authUser?<Navigate to="/form"/>:<Login/>}/>
-      <Route  path="/form" element={authUser?<Form/>:<Navigate to="/register" />}/>
-      <Route path='events/:eventId' element={<EventInfo />} />
-     </Routes>
-     <Toaster/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/register"
+          element={authUser ? <Navigate to="/form" /> : <SignUp />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/form" /> : <Login />}
+        />
+        <Route
+          path="/form"
+          element={authUser ? <Form /> : <Navigate to="/register" />}
+        />
+        <Route
+          path="/events"
+          element={<EventPage/>}
+        />
+      </Routes>
+      <Toaster />
     </div>
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
