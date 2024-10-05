d39009bf2137005414f3180020c7eaa757308684
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2024-10-05 14:28:34 +0530

    collab functionality

--- backend/routes/collab.routes.js
+++ backend/routes/collab.routes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { submitForm, UnauthSubmitForm } from "../controllers/collab.controller.js";
-import { protectCollabRoute } from "../middleware/protectRoutes.js";
+import {
+  submitForm,
+  UnauthSubmitForm,
+} from "../controllers/collab.controller.js";
+import {
+  protectCollabRoute,
+  protectRoute,
+} from "../middleware/protectRoutes.js";
 
 const router = express.Router();
 
 // Protected route for form submission
-router.post("/submit", protectCollabRoute, submitForm);
+router.post("/submit", protectRoute, submitForm);
 
 // Unauthorised route handling
 router.post("/unauthorisedcollab", UnauthSubmitForm);
